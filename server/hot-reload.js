7e192529e234ab8a7d7e19c1b97aa875a56a09cd
Jonathan Cancelli <honathan.cancelli.46@gmail.com>
2026-05-10 15:57:51 +0200

    implement retries

--- server/hot-reload.js
+++ server/hot-reload.js
@@ -1,10 +1,17 @@
 (() => {
 	const url = new URL("{{.WebSocketRoute}}", "ws://localhost:{{.Port}}")
 
+	const MAX_RECONNECT_RETRIES = 5
+	let reconnectRetries = 0
+
 	/** @type {WebSocket} */
 	let ws
 
 	function connect() {
+		if (reconnectRetries > MAX_RECONNECT_RETRIES) {
+			console.log("[hot-reload] failed to reconnect (max retries reached)")
+		}
+
 		ws = new WebSocket(url)
 
 		ws.onmessage = (event) => {
@@ -17,6 +24,7 @@
 
 		ws.onopen = () => {
 			console.log("[hot-reload] connected")
+			reconnectRetries = 0
 		}
 
 		ws.onclose = () => {
@@ -27,6 +35,7 @@
 			console.log(`[hot-reload] websocket error: ${event.toString()}`)
 			ws.close()
 			console.log(`[hot-reload] trying to reconnect`)
+			reconnectRetries += 1
 			connect()
 		}
 	}
